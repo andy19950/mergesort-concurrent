a7fd79359f263f3ec2ad10263f7f295acdea111a
Andy <andy19950@gmail.com>
2016-10-11 23:29:03 +0800

    Change the sort object from integer to string and add data_correctness func

--- main.c
+++ main.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
+#include <time.h>
 
 #include "threadpool.h"
 #include "list.h"
 
-#define USAGE "usage: ./sort [thread_count] [input_count]\n"
+#define USAGE "usage: ./sort [thread_count] [file_name]\n"
+#define BUF_SIZ 512
 
 struct {
     pthread_mutex_t mutex;
@@ -23,9 +26,10 @@ llist_t *merge_list(llist_t *a, llist_t *b)
     llist_t *_list = list_new();
     node_t *current = NULL;
     while (a->size && b->size) {
+        int cmp = strcmp(a->head->data, b->head->data);
         llist_t *small = (llist_t *)
-                         ((intptr_t) a * (a->head->data <= b->head->data) +
-                          (intptr_t) b * (a->head->data > b->head->data));
+                         ((intptr_t) a * (cmp <= 0) +
+                          (intptr_t) b * (cmp > 0));
         if (current) {
             current->next = small->head;
             current = current->next;
@@ -140,28 +144,58 @@ static void *task_run(void *data)
     pthread_exit(NULL);
 }
 
+void data_correctness()
+{
+    FILE *result, *answer;
+    result = fopen("output.txt", "r");
+    answer = fopen("words.txt", "r");
+    char a[BUF_SIZ], b[BUF_SIZ];
+    while(fgets(a, sizeof(a), answer) != NULL){
+        fgets(b, sizeof(b), result);
+        assert(!strcmp(a, b) && "ERROR : Some name is not in the linked-list!!");
+    }
+    //printf("CORRECT : Every name is in the linked-list!!\n");
+    fclose(result);
+    fclose(answer);
+}
+
+static double diff_in_second(struct timespec t1, struct timespec t2)
+{
+    struct timespec diff;
+    if (t2.tv_nsec-t1.tv_nsec < 0) {
+        diff.tv_sec  = t2.tv_sec - t1.tv_sec - 1;
+        diff.tv_nsec = t2.tv_nsec - t1.tv_nsec + 1000000000;
+    } else {
+        diff.tv_sec  = t2.tv_sec - t1.tv_sec;
+        diff.tv_nsec = t2.tv_nsec - t1.tv_nsec;
+    }
+    return (diff.tv_sec + diff.tv_nsec / 1000000000.0);
+}
+
+
 int main(int argc, char const *argv[])
 {
+    char *input_file, line[BUF_SIZ];
+    FILE *fp;
+    struct timespec start, end;   
+    double cpu_time;
+
     if (argc < 3) {
         printf(USAGE);
         return -1;
     }
     thread_count = atoi(argv[1]);
-    data_count = atoi(argv[2]);
+    input_file = argv[2];
+    fp = fopen(input_file, "r");
+
     max_cut = thread_count * (thread_count <= data_count) +
               data_count * (thread_count > data_count) - 1;
 
     /* Read data */
     the_list = list_new();
-
-    /* FIXME: remove all all occurrences of printf and scanf
-     * in favor of automated test flow.
-     */
-    printf("input unsorted data line-by-line\n");
-    for (int i = 0; i < data_count; ++i) {
-        long int data;
-        scanf("%ld", &data);
-        list_add(the_list, data);
+    while(fgets(line, sizeof(line), fp) != NULL) {
+        line[strlen(line)-1] = '\0';
+        list_add(the_list, line);
     }
 
     /* initialize tasks inside thread pool */
@@ -170,14 +204,22 @@ int main(int argc, char const *argv[])
     tmp_list = NULL;
     pool = (tpool_t *) malloc(sizeof(tpool_t));
     tpool_init(pool, thread_count, task_run);
+    clock_gettime(CLOCK_REALTIME, &start);
 
     /* launch the first task */
     task_t *_task = (task_t *) malloc(sizeof(task_t));
     _task->func = cut_func;
     _task->arg = the_list;
     tqueue_push(pool->queue, _task);
-
+    
     /* release thread pool */
     tpool_free(pool);
+    clock_gettime(CLOCK_REALTIME, &end);
+    cpu_time = diff_in_second(start, end);
+    printf("%d %.4lf\n", thread_count, cpu_time);
+    fclose(fp);
+
+    /* data correctness */
+    data_correctness();
     return 0;
 }
